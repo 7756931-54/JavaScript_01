e2fe8828eaeafbceb8d58747d1f3aa616c07d751
Astrit <astriterikamc@gmail.com>
2021-02-22 05:01:07 -0500

    Update Ejercicio2.js

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,9 +1,12 @@
-function updatemenu() {
-    if (document.getElementById('responsive-menu').checked == true) {
-      document.getElementById('menu').style.borderBottomRightRadius = '0';
-      document.getElementById('menu').style.borderBottomLeftRadius = '0';
-    }else{
-      document.getElementById('menu').style.borderRadius = '3px';
+function Calcularnumero (){
+
+var numero = document . getElementById ( 'numero' ). value ;
+
+  for(var i=1; i<=numero; i++)
+  {  
+    for( var x=1;x<=i;x=x+1)
+    {
+    document . write (i + " " );
     }
   }
-  
+}
